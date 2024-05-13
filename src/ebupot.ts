dd27a3b472f1ef1c0417037924ffa129b5f6bc27
Wardi <wardi@nusa.net.id>
2024-05-14 06:38:51 +0700

    Initial revision

---
+++ src/ebupot.ts
@@ -0,0 +1,378 @@
+type EbupotExtractedData = {
+  h1: string
+  b1: string
+  b2: string
+  b7: string[]
+  b8: string[]
+  c1: string
+  c3: string
+}
+
+export function getEbupotFormatedSignature(data: string) {
+  if (data.indexOf('FORMULIR BPBS\nH.1\nH.2\nH.3') > -1) {
+    return 'A'
+  }
+  if (data.indexOf('FORMULIR BPBS\nBukti Pemotongan') > -1) {
+    return 'B'
+  }
+  if (data.indexOf('FORMULIR BPBS\nH.1\nNOMOR') > -1) {
+    return 'C'
+  }
+  if (data.trim().length == 0) {
+    return 'Z'
+  }
+  return 'U'
+}
+
+function extractAFormatedEbupot(data: string) {
+  const p = data.indexOf('Dokumen Referensi')
+  const s = data.substring(p)
+  const ret: EbupotExtractedData = {
+    h1: '',
+    b1: '',
+    b2: '',
+    b7: [],
+    b8: [],
+    c1: '',
+    c3: '',
+  }
+
+  let buffer = ''
+  let state = 0
+  for (const line of s.split(/\n/)) {
+    if (state == 0) {
+      state = 1
+      continue
+    }
+    if (state == 1) {
+      ret.b7.push(line)
+      state = 2
+      continue
+    }
+    if (state == 2) {
+      if (line === 'yyyy') {
+        state = 3
+      }
+      continue
+    }
+    if (state == 3) {
+      state = 4
+      if (line === '') {
+        ret.b7.push('', '')
+        continue
+      }
+      const docName = line.slice(0, -8)
+      const docDate = line.slice(-8)
+      ret.b7.push(docName)
+      ret.b7.push(ddmmyyyyToIso(docDate))
+      continue
+    }
+    if (state == 4) {
+      if (line == 'Tanggal') {
+        state = 5
+      }
+      continue
+    }
+    if (state == 5) {
+      ret.b8.push(line)
+      state = 6
+      continue
+    }
+    if (state == 6) {
+      if (line === 'B.9') {
+        ret.b8.push(buffer.length == 0 ? '' : ddmmyyyyToIso(buffer))
+        buffer = ''
+        state = 7
+        continue
+      }
+      buffer = `${buffer}${line}`
+      continue
+    }
+    if (state == 7) {
+      if (line === 'C.1') {
+        state = 8
+      }
+      continue
+    }
+    if (state == 8) {
+      if (line === ':NPWP') {
+        state = 9
+      }
+      continue
+    }
+    if (state == 9) {
+      if (line === 'Nama Wajib PajakC.2:') {
+        state = 10
+        ret.c1 = buffer
+        buffer = ''
+        continue
+      }
+      buffer = `${buffer}${line}`
+      continue
+    }
+    if (state == 10) {
+      if (line === 'mmyyyy') {
+        state = 11
+      }
+      continue
+    }
+    if (state == 11) {
+      if (line === 'C.4') {
+        state = 12
+        ret.c3 = ddmmyyyyToIso(buffer)
+        buffer = ''
+        continue
+      }
+      buffer = `${buffer}${line}`
+      continue
+    }
+    if (state == 12) {
+      if (line.indexOf('Bukti Pemotongan ini.') > -1) {
+        state = 13
+      }
+      continue
+    }
+    if (state == 13) {
+      ret.h1 = line
+      state = 14
+      continue
+    }
+    if (state == 14) {
+      if (line.length > 4) {
+        state = 15
+        ret.b1 = line
+      }
+      continue
+    }
+    if (state == 15) {
+      ret.b2 = line
+      break
+    }
+  }
+  return ret
+}
+
+function extractBFormatedEbupot(data: string) {
+  const p = data.indexOf('PPh Tidak Final')
+  const s = data.substring(p)
+  const ret: EbupotExtractedData = {
+    h1: '',
+    b1: '',
+    b2: '',
+    b7: [],
+    b8: [],
+    c1: '',
+    c3: '',
+  }
+
+  let state = 0
+  for (const line of s.split(/\n/)) {
+    if (state < 2) {
+      state += 1
+      continue
+    }
+    if (state == 2) {
+      ret.h1 = line
+      state = 3
+      continue
+    }
+    if (state == 3) {
+      if (line === 'B.1B.2B.3B.4B.5B.6') {
+        state = 4
+      }
+      continue
+    }
+    if (state == 4) {
+      state = 5
+      ret.b1 = line.slice(-11, -10) === ',' ? line.slice(-7) : line.slice(-6)
+      const p = line.indexOf('-')
+      ret.b2 = line.substring(p - 2, p + 7)
+      continue
+    }
+    if (state == 5) {
+      if (line === 'ddmmyyyy') {
+        state = 6
+      }
+      continue
+    }
+    if (state == 6) {
+      if (line === 'B.8') {
+        state = 8
+        ret.b7.push('', '', '')
+        continue
+      }
+      state = 7
+      ret.b7.push(line)
+      continue
+    }
+    if (state == 7) {
+      state = 8
+      const randDate = line.slice(-8)
+      const date = [
+        randDate[1],
+        randDate[2],
+        randDate[7],
+        randDate[5],
+        '-',
+        randDate[3],
+        randDate[4],
+        '-',
+        randDate[0],
+        randDate[6],
+      ]
+      ret.b7.push(line.slice(0, -8))
+      ret.b7.push(date.join(''))
+      continue
+    }
+    if (state == 8) {
+      // FIXME
+      state = 9
+      continue
+    }
+    if (state == 9) {
+      if (line.startsWith('C. IDENTITAS PEMOTONG')) {
+        state = 10
+      }
+      continue
+    }
+    if (state == 10) {
+      state = 11
+      const npwp = [
+        line[0],
+        line[14],
+        line[11],
+        line[12],
+        line[13],
+        line[5],
+        line[9],
+        line[3],
+        line[1],
+        line[4],
+        line[2],
+        line[10],
+        line[6],
+        line[7],
+        line[8],
+      ]
+      ret.c1 = npwp.join('')
+      continue
+    }
+    if (state == 11) {
+      if (line == 'C.2Nama Wajib Pajak:') {
+        state = 12
+      }
+      continue
+    }
+    if (state == 12) {
+      const date = [
+        line[1],
+        line[7],
+        line[2],
+        line[4],
+        '-',
+        line[0],
+        line[6],
+        '-',
+        line[5],
+        line[3],
+      ]
+      ret.c3 = date.join('')
+      break
+    }
+  }
+  return ret
+}
+
+function extractCFormatedEbupot(data: string) {
+  const p = data.indexOf('Bukti Pemotongan ini.')
+  const s = data.substring(p)
+  const ret: EbupotExtractedData = {
+    h1: '',
+    b1: '',
+    b2: '',
+    b7: [],
+    b8: [],
+    c1: '',
+    c3: '',
+  }
+
+  let state = 0
+  for (const line of s.split(/\n/)) {
+    if (state == 0) {
+      state = 1
+      continue
+    }
+    if (state == 1) {
+      ret.h1 = line.replace(/\s/g, '')
+      state = 2
+      continue
+    }
+    if (state < 6) {
+      state += 1
+      continue
+    }
+    if (state == 6) {
+      state = 7
+      const p = line.indexOf('-')
+      ret.b1 = line.substring(0, p + 5)
+      ret.b2 = line.substring(p + 5, p + 14)
+      continue
+    }
+    if (state == 7) {
+      state = 8
+      continue
+    }
+    if (state == 8) {
+      state = 9
+      let i = line.length - 1
+      let date = []
+      while (i > 0) {
+        if (line[i] !== ' ') {
+          date.push(line[i])
+        }
+        if (date.length >= 8) {
+          break
+        }
+        i--
+      }
+      date.reverse()
+      ret.b8.push(line.substring(0, i))
+      ret.b8.push(ddmmyyyyToIso(date.join('')))
+      continue
+    }
+    if (state == 9) {
+      state = 10
+      ret.c1 = line.replace(/\s/g, '')
+      continue
+    }
+    if (state == 10) {
+      state = 11
+      continue
+    }
+    if (state == 11) {
+      ret.c3 = ddmmyyyyToIso(line.replace(/\s/g, ''))
+      break
+    }
+  }
+  return ret
+}
+
+export function extractEbupot(data: string, format: string) {
+  switch (format) {
+    case 'A':
+      return extractAFormatedEbupot(data)
+    case 'B':
+      return extractBFormatedEbupot(data)
+    case 'C':
+      return extractCFormatedEbupot(data)
+    default:
+      return {}
+  }
+}
+
+function ddmmyyyyToIso(ddmmyyyy: string) {
+  const year = ddmmyyyy.substring(4, 8)
+  const month = ddmmyyyy.substring(2, 4)
+  const day = ddmmyyyy.substring(0, 2)
+  return `${year}-${month}-${day}`
+}
---
+++ src/main.ts
@@ -0,0 +1,166 @@
+// Imports and Environment Configuration
+import Fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
+import fs from 'fs'
+import { promises as fsPromises } from 'fs'
+import { config } from 'dotenv'
+import path from 'path'
+import os from 'os'
+import pdfParse from 'pdf-parse'
+import axios from 'axios'
+import {
+  extractEbupot,
+  getEbupotFormatedSignature,
+} from './ebupot'
+
+// Load and verify environment variables
+config()
+const environment = {
+  port: process.env.PORT || 3000,
+  username: process.env.NIS_USERNAME!,
+  password: process.env.NIS_PASSWORD!,
+  baseUrl: process.env.NIS_BASE_URL!,
+  loginPath: process.env.NIS_LOGIN_PATH!,
+  mainRoute: process.env.MAIN_ROUTE!,
+  apiKeys: JSON.parse(process.env.API_KEYS || '[]'),
+}
+verifyEnvironment(environment)
+
+// Function to verify required environment variables
+function verifyEnvironment(env: any): void {
+  const required = [
+    env.username,
+    env.password,
+    env.baseUrl,
+    env.loginPath,
+    env.mainRoute,
+  ]
+  if (required.some((value) => !value)) {
+    console.error('Missing required environment variables')
+    process.exit(1)
+  }
+}
+
+// Authentication and File Operations
+let authCookie: string | null = null
+
+async function retrieveAuthenticationCookie(): Promise<string> {
+  const loginUrl = `${environment.baseUrl}${environment.loginPath}`
+  const credentials = new URLSearchParams({
+    userId: environment.username,
+    pass: environment.password,
+    submit: 'Login',
+  })
+
+  const response = await axios.post(loginUrl, credentials, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    maxRedirects: 0,
+    validateStatus: (status) => status >= 200 && status < 400,
+  })
+
+  if (
+    response.headers['set-cookie'] &&
+    response.headers['set-cookie'].length > 0
+  ) {
+    authCookie = response.headers['set-cookie'][0]
+    return authCookie
+  }
+
+  throw new Error('Failed to retrieve authentication cookie')
+}
+
+async function downloadFile(url: string, destination: string): Promise<void> {
+  // Helper function to make the HTTP request
+  async function makeRequest() {
+    const response = await axios.get(url, {
+      headers: { Cookie: authCookie },
+      responseType: 'stream',
+    })
+
+    // If the response status is not OK, refresh the cookie and retry once
+    if (response.status !== 200) {
+      await retrieveAuthenticationCookie() // Refresh the cookie
+      const retryResponse = await axios.get(url, {
+        headers: { Cookie: authCookie },
+        responseType: 'stream',
+      })
+
+      if (retryResponse.status !== 200) {
+        throw new Error(
+          `Failed to download file after retry: server responded with status code ${retryResponse.status}`,
+        )
+      }
+      return retryResponse
+    }
+
+    return response
+  }
+
+  if (!authCookie) {
+    await retrieveAuthenticationCookie()
+  }
+
+  const response = await makeRequest()
+
+  const writer = fs.createWriteStream(destination)
+  response.data.pipe(writer)
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', (err) => {
+      // Handle any errors during the writing process
+      reject(err)
+    })
+  })
+}
+
+// Fastify Setup and Routes
+const server = Fastify({ logger: true })
+
+async function authenticateRequest(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const apiKey = request.headers['x-api-key']
+  if (!apiKey || !environment.apiKeys.includes(apiKey)) {
+    reply.code(401).send({ error: 'Unauthorized' })
+  }
+}
+
+async function handleMainRoute(
+  request: FastifyRequest,
+  reply: FastifyReply,
+): Promise<void> {
+  const fileUrl =
+    environment.baseUrl + request.url.substring(environment.mainRoute.length)
+  const tempDir = await fsPromises.mkdtemp(path.join(os.tmpdir(), 'download-'))
+  const tempFilePath = path.join(tempDir, 'file.tmp')
+  try {
+    await downloadFile(fileUrl, tempFilePath)
+    const pdfBuffer = await fsPromises.readFile(tempFilePath)
+    const pdfData = await pdfParse(pdfBuffer)
+    const format = getEbupotFormatedSignature(pdfData.text)
+    const data = extractEbupot(pdfData.text, format)
+    reply.send({ format, data })
+  } catch (error: any) {
+    console.error(`Download or PDF parsing failed: ${error.message}`)
+    reply.status(500).send({ error: 'Error processing request' })
+  } finally {
+    await fsPromises.unlink(tempFilePath)
+    await fsPromises.rmdir(tempDir)
+  }
+}
+
+function setupRoutes(server: FastifyInstance): void {
+  server.get('*', handleMainRoute)
+  server.get('/', (_req, reply) => reply.send('Server is running'))
+}
+
+// Initialize and start the server
+server.addHook('preHandler', authenticateRequest)
+setupRoutes(server)
+server.listen({ port: +environment.port, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    console.error('Error starting server:', err)
+    process.exit(1)
+  }
+})
